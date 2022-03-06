63130ac3b4b4c0a418a3bfb2831512538b3aff56
Zaute Km <77600757+ZauteKm@users.noreply.github.com>
2022-03-06 23:47:04 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,8 +1,7 @@
 echo "Cloning Repo, Please Wait..."
-git clone -b master https://github.com/ZauteKm/TorrentLeechBot /TorrentLeechBot
-cd /TorrentLeechBot
+git clone -b master https://github.com/ZauteKm/Leech-Bot /Leech-Bot
+cd /Leech-Bot
 echo "Installing Requirements..."
 pip3 install -U -r requirements.txt
 echo "Starting Bot, Please Wait..."
-chmod +x extract
 python3 -m tobrot
