28388fc550aaba8eccae1d262e20726a03d3a912
Zaute Km <77600757+ZauteKm@users.noreply.github.com>
2022-03-06 15:40:45 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,9 @@
-if [[ -n $RCLONE_CONFIG ]]; then
- echo "Rclone config detected"
- echo -e "$RCLONE_CONFIG" > /app/rclone.conf
+echo "Cloning Repo, Please Wait..."
+git clone -b master https://github.com/ZauteKm/TorrentLeechBot /TorrentLeechBot
+cd /TorrentLeechBot
+echo "Installing Requirements..."
+pip3 install -U -r requirements.txt
+echo "Starting Bot, Please Wait..."
 fi
 chmod +x extract
 python3 -m tobrot
